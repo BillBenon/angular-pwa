c5786205631ae38b86662ed25a0c28c3adc6561f
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-07-10 13:14:50 +0200

    added clear and nice angular pagination using ng add @ng-bootstrap/ng-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,31 @@ import { HttpClient } from '@angular/common/http';
 
 import { Post } from './post.model';
 
+const FILTER_BAG_REGEX = /[^0-9]/g;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  page = 4;
+  pageSize = 10;
   posts: Post[] = [];
 
-  constructor(private http: HttpClient) {}
+  getPageSymbol(current: number) {
+    return ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'][current - 1];
+  }
+
+  selectPage(page: string) {
+    this.page = parseInt(page, 10) || 1;
+  }
+
+  formatInput(input: HTMLInputElement) {
+    input.value = input.value.replace(FILTER_BAG_REGEX, '');
+  }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.http
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -9,8 +11,13 @@ import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, PostComponent],
-  imports: [BrowserModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    NgbModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
